69f2b3b20b6baab41a7bf3daded27131eebcf0bb
Matthew Sullivan <matthew.sullivan@hp.com>
2018-08-13 10:18:26 +0100

    basic app

---
+++ BasicApp/MainViewController.h
@@ -0,0 +1,12 @@
+//
+//  MainViewController.h
+//  BasicApp
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+
+@end
+
---
+++ BasicApp/MainViewController.m
@@ -0,0 +1,15 @@
+//
+//  MainViewController.m
+//  BasicApp
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *_outputLabel;
+
+@end
+
+@implementation MainViewController
+
+@end
